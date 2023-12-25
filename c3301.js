f3a2bc1932e3283be624c48fe406a84d802e2383
rmadd06 <rmadds806@gmail.com>
2023-12-25 22:13:42 +0000

    New js and other

---
+++ c3301.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const progressElem = document.querySelector('#progress')
+
+    function scrollHandler() {
+        const scrollY = Math.round(window.scrollY)
+        const maxScrollHeight = document.body.scrollHeight - window.innerHeight
+
+        const progress = (scrollY / maxScrollHeight) * 100
+
+        progressElem.value = progress
+    }
+
+    scrollHandler()
+
+    window.addEventListener('scroll', scrollHandler)
+ });
+
+ document.addEventListener('DOMContentLoaded', function () {
+    const scrollButton = document.getElementById('scrollButton');
+
+    scrollButton.addEventListener('click', function () {
+        const nextSection = document.getElementById('section2');
+
+        // Scroll to the next section
+        nextSection.scrollIntoView({ behavior: 'smooth' });
+    });
+});
+
